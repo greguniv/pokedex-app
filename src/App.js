d4d4ff7d5c8189ac4fce36d0f5c7b08beedb638f
GREGORY HERNANDEZ UNIVERSE <MAICMP8@HOTMAIL.COM>
2022-01-25 11:30:38 -0600

    added user context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import { useContext } from 'react';
 import Nav from './components/Nav'
+// Contexts
+import UserContext from './contexts/UserContext'
 
 function App() {
+  // In order for us to use our context, we import first, then we can use the useContext hook to access our context
+  // const user = useContext(UserContext)
+  // console.log(user)
+
   return (
     <div className="App">
-     
-      
-      <Nav />
+      {/* All context comes with the Provider Component. This allows us to use this as a wrapper and share information with all of its children. We need the value prop inside our provider. */}
+      <UserContext.Provider value={'Christina'}>
+        <Nav />
+      </UserContext.Provider>
     </div>
   );
 }
---
+++ src/contexts/UserContext.js
@@ -0,0 +1,10 @@
+// import createContext
+import React, { createContext } from 'react'
+
+// Simplest way to create a context
+// Standard Practice: name your context the same as your file
+const UserContext = createContext('Teo')
+
+// console.log(UserContext)
+
+export default UserContext
