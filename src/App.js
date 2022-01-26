39e72ab135069290b9e302521a152fdb1c715345
GREGORY HERNANDEZ UNIVERSE <MAICMP8@HOTMAIL.COM>
2022-01-26 08:59:40 -0600

    created home and pokemon list components

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { Routes, Route } from 'react-router-dom'
 import Nav from './components/Nav'
 // Pages
 import Login from './pages/Login'
+import Home from './pages/Home';
+import PokemonList from './pages/PokemonList';
 // Contexts
 import UserContext from './contexts/UserContext'
 
-function App() {
+const App = () => {
   // In order for us to use our context, we import first, then we can use the useContext hook to access our context
   // const user = useContext(UserContext)
   // console.log(user)
@@ -25,8 +27,11 @@ function App() {
         {/* We need to wrap our all of our Routes inside react router Routes component */}
 
         <Routes>
+          <Route path='/' element={<Home />}/>
           <Route path='login' element={<Login setUser={setUser} />} />
+          <Route path='pokemon/list' element={<PokemonList />} />
         </Routes>
+
       </UserContext.Provider>
     </div>
   );
---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            Landing Page for Home!
+        </div>
+    );
+}
+
+export default Home;
