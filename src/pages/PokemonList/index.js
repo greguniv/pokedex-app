7dba581f3bafedcb760e126d4eb20149ecd78b83
GREGORY HERNANDEZ UNIVERSE <MAICMP8@HOTMAIL.COM>
2022-01-27 09:23:11 -0600

    updated pokelist css

--- src/pages/PokemonList/index.js
+++ src/pages/PokemonList/index.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
 import ReactPaginate from 'react-paginate'
 import axios from 'axios'
+import './styles.css'
 
 const PokemonList = ({ pokeList, itemsPerPage }) => {
     // console.log('props', pokeList)
     // We start with an empty list of pokeList.
-    const [currentPokemon, setCurrentPokemon] = useState(null);
+    const [currentPokemon, setCurrentPokemon] = useState([]);
     const [pageCount, setPageCount] = useState(0);
     // Here we use item offsets; we could also use page offsets
     // following the API or data you're working with.
@@ -14,50 +15,62 @@ const PokemonList = ({ pokeList, itemsPerPage }) => {
     useEffect(() => {
         try {
             // Fetch pokeList from another resources.
-        const endOffset = itemOffset + itemsPerPage;
-        console.log(`Loading pokeList from ${itemOffset} to ${endOffset}`);
+            const endOffset = itemOffset + itemsPerPage;
+            console.log(`Loading pokeList from ${itemOffset} to ${endOffset}`);
 
-        const pokeURLS = []
+            const pokeURLs = []
 
-        for (let i = itemOffset; i <= endOffset; i++) {
-            pokeURLs.push(`https://pokeapi.co/v2/pokemon/${i}`)
-        }
+            for (let i = itemOffset + 1; i <= endOffset; i++) {
+                pokeURLs.push(`https://pokeapi.co/api/v2/pokemon/${i}`)
+            }
 
-        console.log('urls', pokeURLs)
+            // console.log('urls', pokeURLs)
+            currPagePokemon(pokeURLs)
 
-        // setCurrentPokemon(pokeList.slice(itemOffset, endOffset));
-        // if(currentPokemon) currPagePokeon()
-        setPageCount(Math.ceil(pokeList.length / itemsPerPage));
-        } catch(error) {
+            // setCurrentPokemon(pokeList.slice(itemOffset, endOffset));
+            // if(currentPokemon) currPagePokeon()
+            const length = pokeList.length ? pokeList.length : 1118
+            setPageCount(Math.ceil(length / itemsPerPage));
+        } catch (error) {
             console.log(error)
         }
     }, [itemOffset, itemsPerPage]);
 
-const currPagePokemon = (pokeURLs) => {
+    const currPagePokemon = (pokeURLs) => {
         try {
             // axios all() makes all concurrent requests
             // instead of doing individual requests, we can programatically make mulptiple requests
             // if one of our Promises fails, the entire request fails
-            axios.all()
-        } catch(error) {
+
+            const pokeArr = []
+            axios.all(pokeURLs.map(async (url) => {
+                const response = await axios.get(url)
+                // console.log(response.data)
+                pokeArr.push(response.data)
+                setCurrentPokemon([pokeArr].flat())
+            }))
+        } catch (error) {
             console.log(error)
         }
     }
 
     const Pokemon = () => {
         return (
-            <>
+            <div className='pokemon-container'>
                 {
-                    pokeList &&
-                    pokeList.map(pokemon => (
-                        <div>
-                            {/* <h3>{pokemon.name}</h3> */}
-
+                    currentPokemon &&
+                    currentPokemon.map(pokemon => (
+                        <div className="card poke-card" key={pokemon.id}>
+                            <img src={pokemon.sprites.front_default} className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">{pokemon.name}</h5>
+                                    <p className="card-text">Order: {pokemon.id}</p>
+                                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                                </div>
                         </div>
-                    )
-                    )
+                    ))
                 }
-            </>
+            </div>
         );
     }
 
@@ -94,7 +107,7 @@ const currPagePokemon = (pokeURLs) => {
                 breakLinkClassName="page-link"
                 containerClassName="pagination"
                 activeClassName="active"
-                renderOnZeroPageCount={null}
+            // renderOnZeroPageCount={null}
             />
         </div>
     );
